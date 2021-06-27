627fab4c7e70b29f45ea25c4d557f2359afbc26e
Kirill Botalov <cccombo.breaker.yo@gmail.com>
2021-06-27 17:57:38 +0300

    Домашнее задание 5 - Cucumber. Выполнил Боталов Кирилл

--- src/test/java/stepdefs/MyStepdefs.java
+++ src/test/java/stepdefs/MyStepdefs.java
@@ -7,6 +7,7 @@ import io.cucumber.java.ru.И;
 import io.cucumber.java.ru.Пусть;
 import io.cucumber.java.ru.Тогда;
 import io.qameta.allure.Allure;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
 import net.bytebuddy.TypeCache;
 import org.openqa.selenium.OutputType;
@@ -15,10 +16,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import pages.MainPage;
 import pages.PrinterPage;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
 import stepdefs.Category;
 import stepdefs.SortBy;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -29,68 +36,86 @@ public class MyStepdefs{
     public PrinterPage printer = PageFactory.initElements(driver, PrinterPage.class);
 
     public void screen() {
-        Allure.addAttachment("Скриншот", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+//        Allure.addAttachment("Скриншот", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+
+    }
+
+    @Attachment(value = "Скриншот", type = "image/png")
+    public byte[] screenshot() {
+
+            BufferedImage bufferedImage = new AShot().takeScreenshot(driver).getImage();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(bufferedImage, "png", baos);
+            baos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+            byte[] image = baos.toByteArray();
+            return image;
 
     }
     @Step("Открываем авито")
     @Пусть("открыт ресурс авито")
     public void открытРесурсАвито() {
         main.goTo();
-        screen();
+        screenshot();
     }
     @Step("Выбираем категорию оргтехника")
     @И("в выпадающем списке категорий выбрана {category}")
     public void вВыпадающемСпискеКатегорийВыбранаОргтехника(Category category) {
         main.selectCategory(category);
         screen();
+        screenshot();
     }
     @Step("В после поиска вводим {text}")
     @И("в поле поиска введено значение {string}")
     public void вПолеПоискаВведеноЗначение(String text) {
         main.sendText(text);
-        screen();
+        screenshot();
     }
     @Step("Кликаем по списку регионов")
     @Тогда("кликнуть по выпадающему списку регионов")
     public void кликнутьПоВыпадающемуСпискуРегионов() {
         main.cityField();
-        screen();
+        screenshot();
     }
     @Step("Вводим в поле регион значение {city}")
     @Тогда("в поле регион введено значение {string}")
     public void вПолеРегионВведеноЗначение(String city) {
         main.chooseCity(city);
-        screen();
+        screenshot();
     }
     @Step("Нажимаем кнопку показать объявления")
     @И("нажата кнопка показать объявления")
     public void нажатаКнопкаПоказатьОбъявления() {
         main.buttonClick();
-        screen();
+        screenshot();
     }
     @Step("Открылась страница результаты по запросу {text}")
     @Тогда("открылась страница результаты по запросу {string}")
     public void открыласьСтраницаРезультатыПоЗапросуПринтер(String text) {
         printer.checkPageTitle(text);
-        screen();
+        screenshot();
     }
     @Step("Активирован чекбокс только с фотографией")
     @И("активирован чекбокс только с фотографией")
     public void активированЧекбоксТолькоСФотографией() {
         printer.checkboxAndSearch();
-        screen();
+        screenshot();
     }
     @Step("В выпадающем списке сортировка выбрано значение дороже")
     @И("в выпадающем списке сортировка выбрано значение {sort}")
     public void вВыпадающемСпискеСортировкаВыбраноЗначениеДороже(SortBy by) {
         printer.sortBy(by);
-        screen();
+        screenshot();
     }
     @Step("В консоль выведено значение названия и цены {number} первых товаров")
     @И("в консоль выведено значение названия и цены {int} первых товаров")
     public void вКонсольВыведеноЗначениеНазванияИЦеныПервыхТоваров(int number) {
         printer.takeExpensivePrinter(number);
-        screen();
+        screenshot();
     }
 
     @ParameterType(".*")
